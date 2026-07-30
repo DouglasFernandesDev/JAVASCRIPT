6724063de07a79943b2aaa479708de8b1a930d8a
DouglasFernandesDev <douglasfernandes855@gmail.com>
2026-07-29 23:31:39 -0300

    Create cursos_idiomas.js

---
+++ cursos_idiomas.js
@@ -0,0 +1,11 @@
+let cursos_idiomas = [
+    { nome: "Inglês", preco: 2500, carga_horaria: 160 },
+    { nome: "Espanhol", preco: 1500, carga_horaria: 110 },
+    { nome: "Francês", preco: 3600, carga_horaria: 200 },
+    { nome: "Chinês", preco: 5500, carga_horaria: 400 },
+    { nome: "Alemão", preco: 3800, carga_horaria: 230 }
+];
+
+for(let cursos of cursos_idiomas){
+    
+}
